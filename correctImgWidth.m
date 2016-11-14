6eb656d0a8775dce45ae4fbb4d14655285189670
Richard Kirby <richard@kspresearch.com>
2016-11-14 09:13:14 -0700

    Added error message if using a version of MATLAB without affine2d function.

--- correctImgWidth.m
+++ correctImgWidth.m
@@ -7,12 +7,15 @@ img = imcrop(img,[64, 1, 511, 480]);
 
 A = [640/512, 0 , 0 ; 0, 1, 0 ; 0, 0, 1];
 
-tform = affine2d(A);
-
-imgOut = imwarp(img, tform);
-
-imtool(imgOut)
-
+    try
+        tform = affine2d(A);
+        imgOut = imwarp(img, tform);
+        imtool(imgOut)
+    catch
+        disp('Requires MATLABE 2013b or later')
+    end
+    
+    imgOut = 0;
 
 end
 
